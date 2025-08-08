edbb51fa9974cddcbc4067591477d56437aacc85
Maddie Kol Roberts <madkroberts2309@gmail.com>
2025-08-08 22:17:47 +0000

    update to code

--- organizedcode.py
+++ organizedcode.py
@@ -2,15 +2,20 @@ import sqlite3
 import xml.etree.ElementTree as ET
 import requests
 import os
+import time
 
 
 def downloadfiles(year, num):
-    if fileexists(year, num):
-        break
+    
+    sc = 0
+
     #make url 
     urlnum = str(num).zfill(3)
     urlyear = f"https://clerk.house.gov/evs/{year}/roll"
     url = f"{urlyear}{urlnum}.xml"
+    
+    if fileexists(year, urlnum):
+        return "already done"
 
     #make file name
     filename = f"xmlfiles/{year}{urlnum}.xml"
@@ -20,10 +25,15 @@ def downloadfiles(year, num):
         
         #finds if anything screwed up
         response = requests.get(url, stream=True)
-        response.raise_for_status()
-        print(response)
-        
+        #response.raise_for_status()
+        sc = response.status_code
+        print(response.ok)
+        #time.sleep(1)
+        if int(sc) == 403:
+            response.raise_for_status()
+       
         #creates and downloads file
+
         with open(filename, 'wb') as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
@@ -35,15 +45,26 @@ def downloadfiles(year, num):
         with open("failedurls.txt", "a") as file:
             file.write(url+"\n")
 
-        return
-
+        #return
+    return (sc)
 
 
 def fileexists(year, num):
-    os.path.exists(f"xmlfiles/{year}{urlnum}.xml")
+    return os.path.exists(f"xmlfiles/{year}{num}.xml")
 
 
-downloadfiles(2003, 95)
 
-downloadfiles(2060, 1119)
-downloadfiles(1998, 205)
+counter = 0
+for i in range(1990, 2026):
+    for b in range(1,1200):
+        if not fileexists(i, str(b).zfill(3)):
+            sc = downloadfiles(i, b)
+            print (sc)
+            if sc == 403:
+                time.sleep(1)
+                counter = counter+1
+                if counter == 10:
+                    counter = 0
+                    break
+
+            #time.sleep(.75)
